3616dd581b9ef1b7e0c850a8214690d592308bfd
민성홍 <sunghong32@naver.com>
2022-07-20 20:52:42 +0900

    [메인뷰 추가]

---
+++ FIFAGG/FIFAGG/Scenes/Main/MainView.swift
@@ -0,0 +1,15 @@
+//
+//  MainView.swift
+//  FIFAGG
+//
+//  Created by 민성홍 on 2022/07/17.
+//
+
+import UIKit
+import SnapKit
+
+class MainView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
