1b7a08c1b30328679aba63e8c71f13b4941ee896
차요셉 <jscha@raoncorp.com>
2023-01-27 16:56:54 +0900

    fix: 잘못단 주석 및 파일 위치 수정

--- FIFAGG/FIFAGG/Data/Repositories/DefaultMetaInfoRepository.swift
+++ FIFAGG/FIFAGG/Data/Repositories/DefaultMetaInfoRepository.swift
@@ -29,13 +29,18 @@ extension DefaultMetaInfoRepository: MetaInfoRepository {
         let endpoint = APIEndpoints.getSpidEtag(etag: savedEtag)
         
         return Single<Void>.create { [weak self] singleCloser in
+            
             guard let self = self else { return Disposables.create() }
+            
             self.dataTransferService.requestWithEtag(with: endpoint)
                 .subscribe(onSuccess: { (response, isServerDataUpdated, etag) in
+                    
                     UserDefaults.standard.set(etag, forKey: UserDefaultsKey.spidEtag) // 서버로 부터 받은 etag 저장
+                    
                     if isServerDataUpdated { // HTTP StatusCode가 200대면 isServerDataUpdated값은 true, 즉 Realm의 선수정보 업데이트
                         response?.forEach { [weak self] spidDTO in
                             guard let self = self else { return }
+                            
                             self.spidRealmStorage.save(entity: spidDTO)
                                 .subscribe(onNext: {
                                     singleCloser(.success(()))
@@ -44,7 +49,7 @@ extension DefaultMetaInfoRepository: MetaInfoRepository {
                                 })
                                 .disposed(by: self.disposeBag)
                         }
-                    } else { // HTTP StatusCode가 200대면 isServerDataUpdated값은 false, 즉 Realm의 선수정보 업데이트 하지 않음
+                    } else { // HTTP StatusCode가 200대가 아니면 isServerDataUpdated값은 false, 즉 Realm의 선수정보 업데이트 하지 않음
                         singleCloser(.success(()))
                     }
                 }, onFailure: { networkError in
