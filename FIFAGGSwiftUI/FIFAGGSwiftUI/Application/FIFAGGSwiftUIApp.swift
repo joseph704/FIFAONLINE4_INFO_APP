230ebb206c0c23fd63b7e5e7ef73baf6afccf39c
sunghong32 <sunghong32@uangel.com>
2023-07-28 15:44:37 +0900

    chore: SwiftUI 프로젝트 생성 && 서드파티 라이브러리 SPM 설치 && 폴더링

---
+++ FIFAGGSwiftUI/FIFAGGSwiftUI/Application/FIFAGGSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIFAGGSwiftUIApp.swift
+//  FIFAGGSwiftUI
+//
+//  Created by 민성홍 on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct FIFAGGSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
